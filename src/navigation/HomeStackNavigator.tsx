581cb3efc46fd436c61c822cdeb53bea04824ac5
SouliMathieu <soulimathieu65@gmail.com>
2026-09-23 00:34:45 +0100

    feat(solar): register recommendation result route

--- src/navigation/HomeStackNavigator.tsx
+++ src/navigation/HomeStackNavigator.tsx
@@ -6,12 +6,15 @@ import HomeScreen from '../screens/home/HomeScreen';
 import ProductDetailScreen from '../screens/product/ProductDetailScreen';
 import CategoryProductsScreen from '../screens/categories/CategoryProductsScreen';
 import SolarAssistantScreen from '../screens/energy/SolarAssistantScreen';
+import SolarResultScreen from '../screens/energy/SolarResultScreen';
 import { Colors } from '../constants/colors';
 import { Category, Product } from '../types';
+import { SolarAnswers } from '../utils/solarEstimator';
 
 export type HomeStackParamList = {
   HomeMain: undefined;
   SolarAssistant: undefined;
+  SolarResult: { answers: SolarAnswers };
   ProductDetail: { product: Product };
   CategoryProducts: { category: Category };
 };
@@ -22,37 +25,20 @@ export default function HomeStackNavigator() {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerStyle: {
-          backgroundColor: Colors.white,
-        },
+        headerStyle: { backgroundColor: Colors.white },
         headerTintColor: Colors.text,
-        headerTitleStyle: {
-          fontWeight: '800',
-        },
+        headerTitleStyle: { fontWeight: '800' },
         headerShadowVisible: false,
       }}
     >
-      <Stack.Screen
-        name="HomeMain"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="SolarAssistant"
-        component={SolarAssistantScreen}
-        options={{ title: 'Assistant solaire' }}
-      />
-      <Stack.Screen
-        name="ProductDetail"
-        component={ProductDetailScreen}
-        options={{ title: 'Détails du produit' }}
-      />
+      <Stack.Screen name="HomeMain" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="SolarAssistant" component={SolarAssistantScreen} options={{ title: 'Assistant solaire' }} />
+      <Stack.Screen name="SolarResult" component={SolarResultScreen} options={{ title: 'Votre solution solaire' }} />
+      <Stack.Screen name="ProductDetail" component={ProductDetailScreen} options={{ title: 'Détails du produit' }} />
       <Stack.Screen
         name="CategoryProducts"
         component={CategoryProductsScreen}
-        options={({ route }) => ({
-          title: route.params.category.name,
-        })}
+        options={({ route }) => ({ title: route.params.category.name })}
       />
     </Stack.Navigator>
   );
