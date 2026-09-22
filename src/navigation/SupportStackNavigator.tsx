62aa9df58b66bc35961e5f05fbb62ef0539d781b
SouliMathieu <soulimathieu65@gmail.com>
2026-09-23 00:41:36 +0100

    feat(navigation): add support stack

---
+++ src/navigation/SupportStackNavigator.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SupportHomeScreen from '../screens/support/SupportHomeScreen';
+import RepairRequestV2Screen from '../screens/profile/RepairRequestV2Screen';
+import InstallationRequestScreen from '../screens/profile/InstallationRequestScreen';
+import ContactScreen from '../screens/profile/ContactScreen';
+import { Colors } from '../constants/colors';
+
+export type SupportStackParamList = {
+  SupportHome: undefined;
+  RepairRequest: undefined;
+  InstallationRequest: undefined;
+  Contact: undefined;
+};
+
+const Stack = createNativeStackNavigator<SupportStackParamList>();
+
+export default function SupportStackNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.white },
+        headerTintColor: Colors.text,
+        headerTitleStyle: { fontWeight: '800' },
+        headerShadowVisible: false,
+      }}
+    >
+      <Stack.Screen name="SupportHome" component={SupportHomeScreen} options={{ title: 'Assistance' }} />
+      <Stack.Screen name="RepairRequest" component={RepairRequestV2Screen} options={{ title: 'Signaler un problème' }} />
+      <Stack.Screen name="InstallationRequest" component={InstallationRequestScreen} options={{ title: "Demande d'installation" }} />
+      <Stack.Screen name="Contact" component={ContactScreen} options={{ title: 'Nous contacter' }} />
+    </Stack.Navigator>
+  );
+}
