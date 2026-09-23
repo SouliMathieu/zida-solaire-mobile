f8ded725fabc2a0e94a2d7897cbfc44ffd5cd318
SouliMathieu <soulimathieu65@gmail.com>
2026-09-23 01:05:33 +0100

    feat(sav): wire repair ticket history navigation

--- src/navigation/SupportStackNavigator.tsx
+++ src/navigation/SupportStackNavigator.tsx
@@ -4,11 +4,16 @@ import SupportHomeScreen from '../screens/support/SupportHomeScreen';
 import RepairRequestV2Screen from '../screens/profile/RepairRequestV2Screen';
 import InstallationRequestScreen from '../screens/profile/InstallationRequestScreen';
 import ContactScreen from '../screens/profile/ContactScreen';
+import RepairTicketsScreen from '../screens/support/RepairTicketsScreen';
+import RepairTicketDetailScreen from '../screens/support/RepairTicketDetailScreen';
+import { RepairTicket } from '../hooks/useRepairs';
 import { Colors } from '../constants/colors';
 
 export type SupportStackParamList = {
   SupportHome: undefined;
   RepairRequest: undefined;
+  RepairTickets: undefined;
+  RepairTicketDetail: { ticket: RepairTicket };
   InstallationRequest: undefined;
   Contact: undefined;
 };
@@ -27,6 +32,8 @@ export default function SupportStackNavigator() {
     >
       <Stack.Screen name="SupportHome" component={SupportHomeScreen} options={{ title: 'Assistance' }} />
       <Stack.Screen name="RepairRequest" component={RepairRequestV2Screen} options={{ title: 'Signaler un problème' }} />
+      <Stack.Screen name="RepairTickets" component={RepairTicketsScreen} options={{ title: 'Mes tickets SAV' }} />
+      <Stack.Screen name="RepairTicketDetail" component={RepairTicketDetailScreen} options={{ title: 'Détail du ticket' }} />
       <Stack.Screen name="InstallationRequest" component={InstallationRequestScreen} options={{ title: "Demande d'installation" }} />
       <Stack.Screen name="Contact" component={ContactScreen} options={{ title: 'Nous contacter' }} />
     </Stack.Navigator>
